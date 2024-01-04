1ef374d0d882e98ebbb71eed9c0d129b652c5b6e
Ali Yousefi <ali.visual.studio@gmail.com>
2024-01-04 10:01:27 +0330

    save user pass when register

--- src/CSharp/MAUI-Front/AvalonUI/AvalonUI.ViewModels/ViewModels/AvalonRegisterViewModel.cs
+++ src/CSharp/MAUI-Front/AvalonUI/AvalonUI.ViewModels/ViewModels/AvalonRegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using EasyMicroservices.Security;
+﻿using AvalonUI.Helpers;
+using EasyMicroservices.Security;
 using EasyMicroservices.ServiceContracts;
 using EasyMicroservices.UI.Cores.Navigations;
 using EasyMicroservices.UI.Identity.ViewModels.Authentications;
@@ -17,12 +18,20 @@ public class AvalonRegisterViewModel : RegisterViewModel
         var result = await base.Register();
         if (result.IsSuccess)
         {
-            await DisplayError("ثبت نام شما با موفقیت انجام شد.");
+            ApplicationDataManager.Current.ApplicationData.UserName = UserName;
+            ApplicationDataManager.Current.ApplicationData.Password = Password;
+            await ApplicationDataManager.Current.Save();
+
+            await DisplayAlert("موفق", "ثبت نام شما با موفقیت انجام شد.", "باشه");
             await NavigationManagerBase.Current.PopAsync();
         }
         else if (result.Error.FailedReasonType == EasyMicroservices.ServiceContracts.FailedReasonType.Duplicate)
         {
             await DisplayError("نام کاربری قبلا توسط شخص دیگری انتخاب شده است، لطفا نام کاربری دیگری انتخاب کنید و مجدد دکمه‌ی ثبت نام را کلیک کنید.");
+        }
+        else
+        {
+            await DisplayError(GetLanguage(result.Error.Message));
         }
         return result;
     }
